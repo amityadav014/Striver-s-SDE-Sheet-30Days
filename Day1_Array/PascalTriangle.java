2b74f445d82b6e1b5fed79825aa58835bc5f9d75
amityadav014 <147026487+amityadav014@users.noreply.github.com>
2023-10-07 10:38:07 +0530

    PascalTriangle code

--- Day1_Array/PascalTriangle.java
+++ Day1_Array/PascalTriangle.java
@@ -1,4 +1,23 @@
-package Day1_Array;
+//problem link : https://leetcode.com/problems/pascals-triangle/
 
-public class PascalTriangle {
+
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+
+        if(numRows==0) return new ArrayList();
+        List<List<Integer>> result=new ArrayList();
+        for(int i=1;i<=numRows;i++){
+            List<Integer> row=new ArrayList();
+            for(int j=0;j<i;j++){
+                //  System.out.println(j);
+                if(j==0||j==i-1){
+                    row.add(1);
+                }else{
+                    row.add(result.get(i-2).get(j)+result.get(i-2).get(j-1));
+                }
+            }
+            result.add(row);
+        }
+        return result;
+    }
 }
