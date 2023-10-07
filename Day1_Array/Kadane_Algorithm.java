c0395ac528e7a8c4cb12e2793b6309b938e809e3
amityadav014 <147026487+amityadav014@users.noreply.github.com>
2023-10-07 13:15:06 +0530

    Kadane algo + next permutation code

--- Day1_Array/Kadane_Algorithm.java
+++ Day1_Array/Kadane_Algorithm.java
@@ -1,4 +1,22 @@
-package Day1_Array;
 
-public class Kadane_Algorithm {
+//problem link : https://leetcode.com/problems/maximum-subarray/
+
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+
+        int maxSum = nums[0]; // Initialize the maximum sum with the first element of the array
+        int currentSum = nums[0]; // Initialize the current sum with the first element
+
+        for (int i = 1; i < nums.length; i++) {
+            // If the current element is greater than the sum ending at the previous element, start a new subarray
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+
+            // Update the maximum sum if the current sum is greater
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
 }
+
