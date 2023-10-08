be57c76a4b51f8f01e493240a4bf4ab6c3be052f
amityadav014 <147026487+amityadav014@users.noreply.github.com>
2023-10-08 22:25:45 +0530

    sort code 012 code

--- Array/Sort_an_array_of_0s_1s_and_2s.java
+++ Array/Sort_an_array_of_0s_1s_and_2s.java
@@ -1,4 +1,40 @@
-package Day1_Array;
 
-public class Sort_an_array_of_0s_1s_and_2s {
+//problem link : https://leetcode.com/problems/sort-colors/
+
+
+
+class Solution {
+    public void sortColors(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+        int mid = 0;
+        int temp;
+
+        while (mid <= high) {
+            switch (nums[mid]) {
+                case 0: {
+                    temp = nums[low];
+                    nums[low] = nums[mid];
+                    nums[mid] = temp;
+                    low++;
+                    mid++;
+                    break;
+                }
+                case 1: {
+                    mid++;
+                    break;
+                }
+                case 2: {
+                    temp = nums[mid];
+                    nums[mid] = nums[high];
+                    nums[high] = temp;
+                    high--;
+                    break;
+                }
+            }
+        }
+    }
 }
+
+
+
