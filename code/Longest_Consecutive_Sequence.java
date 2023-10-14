1c24438b96c7b23290820dcdf0f54e89d7be68a8
amityadav014 <147026487+amityadav014@users.noreply.github.com>
2023-10-14 21:24:37 +0530

    code

---
+++ code/Longest_Consecutive_Sequence.java
@@ -0,0 +1,28 @@
+//problem link:https://leetcode.com/problems/longest-consecutive-sequence/description/
+
+class Solution{
+    public int longestConsecutive(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+        int longestStreak = 0;
+        for (int num : set) {
+            if (!set.contains(num - 1)) {
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (set.contains(currentNum + 1)) {
+                    currentNum++;
+                    currentStreak++;
+                }
+                longestStreak = Math.max(longestStreak, currentStreak);
+            }
+        }
+
+        return longestStreak;
+    }
+}
