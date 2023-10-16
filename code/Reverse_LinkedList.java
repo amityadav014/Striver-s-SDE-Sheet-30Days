a7fca67640b707b5844b1474dba75e8804069347
amityadav014 <147026487+amityadav014@users.noreply.github.com>
2023-10-16 09:49:59 +0530

    day12

---
+++ code/Reverse_LinkedList.java
@@ -0,0 +1,27 @@
+//problem link:https://leetcode.com/problems/reverse-linked-list/
+
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+        while (current != null) {
+            ListNode nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+        return prev;
+    }
+}
