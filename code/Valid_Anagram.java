f4290965578d0c49df45fcaac921aba081a96381
amityadav014 <147026487+amityadav014@users.noreply.github.com>
2023-10-29 22:35:35 +0530

    day 25

---
+++ code/Valid_Anagram.java
@@ -0,0 +1,19 @@
+//problem link : https://leetcode.com/problems/valid-anagram/
+
+
+
+public class Solution {
+  public boolean isAnagram(String s, String t) {
+    if (s.length() != t.length()) {
+      return false;
+    }
+
+    char[] sArray = s.toCharArray();
+    char[] tArray = t.toCharArray();
+
+    Arrays.sort(sArray);
+    Arrays.sort(tArray);
+
+    return Arrays.equals(sArray, tArray);
+  }
+}
